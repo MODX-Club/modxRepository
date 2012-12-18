6cb65f9546aefc7f3d20997cda2ac07212b57a20
Николай <info@newpg.ru>
2012-12-18 11:21:14 +0400

    minor

--- _build/data/transport.settings.php
+++ _build/data/transport.settings.php
@@ -9,15 +9,25 @@
 global  $modx, $sources;
 $settings = array();
 
-$settings['modxRepository.template'] = $modx->newObject('modSystemSetting');
+/*$settings['modxRepository.template'] = $modx->newObject('modSystemSetting');
 $settings['modxRepository.template']->fromArray(array(
     'key' => 'modxRepository.template',
     'value' => 'default',
     'xtype' => 'textfield',
     'namespace' => 'modxrepository',
     'area' => 'site',
+),'',true,true);*/
+
+
+$settings['modxRepository.request_path'] = $modx->newObject('modSystemSetting');
+$settings['modxRepository.request_path']->fromArray(array(
+    'key' => 'modxRepository.request_path',
+    'value' => 'extras/',
+    'xtype' => 'textfield',
+    'namespace' => 'modxrepository',
+    'area' => 'site',
 ),'',true,true);
+ 
 
-
-
+ 
 return $settings;
